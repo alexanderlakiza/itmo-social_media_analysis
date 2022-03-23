c3d81abdc394261eb3f9635d886c32b3674f340d
alexanderlakiza <alelakiza@yandex.ru>
2022-03-23 21:22:35 +0300

    lab1 is started

--- ordinary_subject/lab1/api_requests.py
+++ ordinary_subject/lab1/api_requests.py
@@ -1,39 +1,81 @@
-from ordinary_subject.config import VK_CONFIG as cfg
+from ordinary_subject.config import VK_CONFIG as vk_cfg
+import pickle
 import requests
+import time
 
 
-# import time
+def get_id_from_nickname(nickname="alexanderlakiza"):
+    query = f"{vk_cfg['vk_api_domain']}/utils.resolveScreenName?screen_name={nickname}&" \
+            f"access_token={vk_cfg['access_token']}&v={vk_cfg['api_version']}"
 
+    response = requests.get(url=query).json()
+    return response['response']['object_id']
 
-# "universe_of_f1", "f1_fun_club"
 
 def get_group_members_count(community_id):
-    query = f"{cfg['vk_api_domain']}/groups.getMembers?" \
+    query = f"{vk_cfg['vk_api_domain']}/groups.getMembers?" \
             f"group_id={community_id}&" \
-            f"access_token={cfg['access_token']}&v={cfg['api_version']}"
+            f"access_token={vk_cfg['access_token']}&v={vk_cfg['api_version']}"
 
     response = requests.get(url=query).json()
-    return response['response']
+    return response['response']['count']
 
 
-def get_user_ids_of_communities(community_id):
-    members_count = get_group_members_count(community_id)
+def get_user_ids_of_communities(group_id):
+    members_count = get_group_members_count(group_id)
     all_users_ids = []
 
-    # for n_iter in range(members_count // 1000 + 1):
-    for n_iter in range(1):
+    query_start = f"{vk_cfg['vk_api_domain']}/execute?" \
+                  f"access_token={vk_cfg['access_token']}&" \
+                  f"v={vk_cfg['api_version']}&code="
+
+    for n_iter in range(members_count // 1000 + 1):
+        # for n_iter in range(1):
         offset = n_iter * 1000
 
-        query = f"{cfg['vk_api_domain']}/execute?" \
-                f"access_token={cfg['access_token']}&" \
-                f"v={cfg['api_version']}&code="
+        code = f"return API.groups.getMembers({{'group_id': {group_id}, 'offset': {offset}}});"
+        query = query_start + code
 
-        code = f"return API.groups.getMembers({{'group_id': {community_id}, 'offset': {offset}}});"
-        query += code
+        response = requests.get(url=query).json()['response']['items']
+        all_users_ids.extend(response)
+        time.sleep(0.35)
 
-    response = requests.get(url=query).json()
-    return response
+    return all_users_ids
+
+
+def get_posts_of_group(group_id):
+    posts_infos = []
+
+    for i in range(20):
+        query = f"{vk_cfg['vk_api_domain']}/wall.get?" \
+                f"owner_id=-{group_id}&offset={i * 100}&count=100&" \
+                f"access_token={vk_cfg['access_token']}&v={vk_cfg['api_version']}"
+
+        response = requests.get(url=query).json()['response']['items']
+        posts_infos.extend(response)
+        time.sleep(0.35)
+
+    return posts_infos
 
 
 if __name__ == "__main__":
-    print(get_user_ids_of_communities("36759319"))
+    ids_of_groups = {"formula1.championat": get_id_from_nickname("formula1.championat"),
+                     "grand_prixf1_ru": get_id_from_nickname("grand_prixf1_ru")}
+
+    members_dict = {"formula1.championat": get_user_ids_of_communities(ids_of_groups["formula1.championat"]),
+                    "grand_prixf1_ru": get_user_ids_of_communities(ids_of_groups["grand_prixf1_ru"])}
+
+    print('Members are collected!')
+
+    posts_dict = {"formula1.championat": get_posts_of_group(ids_of_groups["formula1.championat"]),
+                  "grand_prixf1_ru": get_posts_of_group(ids_of_groups["grand_prixf1_ru"])}
+
+    print('Posts are collected!')
+
+    with open('members_of_groups.pickle', 'wb') as handle:
+        pickle.dump(members_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open('posts_of_groups.pickle', 'wb') as handle:
+        pickle.dump(posts_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print('All data is collected and saved!')
